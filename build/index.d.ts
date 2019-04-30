46c5c8b8894cca5e549f1560199f16e472c2739e
Zane Starr <zcstarr@gmail.com>
2019-04-30 11:04:46 -0700

    include build assets

---
+++ build/index.d.ts
@@ -0,0 +1,40 @@
+import * as jayson from "jayson/promise";
+export declare type TServiceName = string;
+export declare type TVersion = string;
+export declare type TInstallSuccess = boolean;
+export declare type TInstalledServices = string[];
+export declare type TRunningServices = Array<{
+    type?: Array<string | string | string>;
+    [k: string]: any;
+}>;
+export interface IServiceConfig {
+    type?: Array<string | string | string>;
+    host?: string;
+    port?: number;
+    name?: string;
+    environment?: string;
+    [k: string]: any;
+}
+export default class ServiceRunner {
+    rpc: jayson.Client;
+    private validator;
+    private openrpcDocument;
+    constructor(options: any);
+    /**
+     *
+     */
+    installService(serviceName: TServiceName, version: TVersion): Promise<TInstallSuccess>;
+    /**
+     *
+     */
+    listInstalledServices(): Promise<TInstalledServices>;
+    /**
+     *
+     */
+    listRunningServices(): Promise<TRunningServices>;
+    /**
+     *
+     */
+    startService(): Promise<IServiceConfig>;
+    private request;
+}
